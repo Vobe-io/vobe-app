d9f44072ec5da207dc8b77d5abbdd98bc51aa6cf
Comesa <mccomesa@gmail.com>
2020-02-15 19:59:06 +0100

    re-init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Test',
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr
+      )
+    )
+  );
+}
